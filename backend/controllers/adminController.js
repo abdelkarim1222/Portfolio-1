941e6b4ee85f6b1a0f1b996bcdf50baf841b6d21
MarzouguiAkrem <marzougui.akrem.pro@gmail.com>
2024-07-07 13:05:45 +0100

    tester login et register

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -7,15 +7,23 @@ const registerAdmin = async (req, res) => {
     const { firstName, lastName, username, password, avatar } = req.body;
 
     try {
+        // Vérifier si l'administrateur existe déjà
         const existingAdmin = await Admin.findOne({ username });
         if (existingAdmin) {
             return res.status(400).json({ message: 'Username already exists' });
         }
 
-        const newAdmin = new Admin({ firstName, lastName, username, password, avatar });
+        // Hasher le mot de passe
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Créer un nouvel administrateur
+        const newAdmin = new Admin({ firstName, lastName, username, password: hashedPassword, avatar });
         await newAdmin.save();
 
+        // Générer un token JWT
         const token = jwt.sign({ id: newAdmin._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        // Répondre avec le token JWT
         res.status(201).json({ token });
     } catch (err) {
         res.status(500).json({ message: err.message });
@@ -27,12 +35,16 @@ const loginAdmin = async (req, res) => {
     const { username, password } = req.body;
 
     try {
+        // Rechercher l'administrateur dans la base de données
         const admin = await Admin.findOne({ username });
         if (!admin || !(await bcrypt.compare(password, admin.password))) {
             return res.status(401).json({ message: 'Invalid username or password' });
         }
 
+        // Générer un token JWT pour l'administrateur authentifié
         const token = jwt.sign({ id: admin._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        // Répondre avec le token JWT
         res.status(200).json({ token });
     } catch (err) {
         res.status(500).json({ message: err.message });
--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -5,7 +5,10 @@ const adminSchema = new mongoose.Schema({
     lastName: { type: String, required: true },
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    avatar: { type: String }
+    avatar: {
+        type: String,
+        default: 'https://imgs.search.brave.com/qckNHIA3-RFIn9aFlH-zipn-lombVlJzDhCjofSw-bg/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/dzNzY2hvb2xzLmNv/bS93M2ltYWdlcy9h/dmF0YXIyLnBuZw' // Définir une URL par défaut pour l'avatar
+    }
 });
 
 module.exports = mongoose.model('Admin', adminSchema);

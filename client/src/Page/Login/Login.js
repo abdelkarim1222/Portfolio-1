1ce3a12df07b8facd172f5fe713416a59a6ccdc9
ezzar abdelkarim <99731689+abdelkarim1222@users.noreply.github.com>
2024-07-12 05:26:29 -0700

    sds

--- client/src/Page/Login/Login.js
+++ client/src/Page/Login/Login.js
@@ -5,25 +5,7 @@ const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const response = await fetch('http://localhost:5000/api/admin/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ username, password }),
-    });
-
-    const data = await response.json();
-    if (response.ok) {
-      localStorage.setItem('token', data.token);
-      alert('Login successful!');
-    } else {
-      alert(data.message || 'Login failed');
-    }
-  };
+ 
 
   return (
     <Container>
@@ -33,8 +15,7 @@ const Login = () => {
           <TextField 
             label="Username" 
             variant="outlined" 
-            fullWidth 
-            margin="normal" 
+            fullWidth             margin="normal" 
             value={username} 
             onChange={(e) => setUsername(e.target.value)} 
             required 
